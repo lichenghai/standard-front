3e7c8fac00ac6b6881700ec02513f78b4b020e1d
lichenghai <haicheng5@126.com>
2018-04-04 13:50:15 +0800

    修改区分微服务后的登录URL

--- master/js/modules/controllers/access-login.js
+++ master/js/modules/controllers/access-login.js
@@ -12,7 +12,7 @@ App.controller('LoginFormController', ['$scope', '$rootScope', '$http', '$state'
  //       $state.go('app.account');
 
        $http
-            .post('/apis/person/login?account=' + $scope.account.username + '&password=' + $scope.account.password)
+            .post('/apis/remove-me/account-service/person/login?account=' + $scope.account.username + '&password=' + $scope.account.password)
             .then(function (response) {
                 if (response.data.status!=200) {
                     $scope.authMsg = response.data.message;
